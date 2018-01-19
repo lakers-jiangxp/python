32e535bc68a4a0614da179b2f301ad2fb5c2bb95
lakers-jiangxp <34905209+lakers-jiangxp@users.noreply.github.com>
2018-01-19 16:22:05 +0800

    Update shop.py

--- shop.py
+++ shop.py
@@ -3,19 +3,19 @@
 # Created		: 2017.12.28
 # Last Modified	: 
 # Version		: 1.0
-#_*_ coding=utf-8 _*_
-shop_list = [['apple',2000],['bake',2222],['shoes',800],]
+# _*_ coding=utf-8 _*_
+shop_list = [['apple', 2000], ['bake', 2222], ['shoes', 800], ]
 # slary = 9000 
 slary = input("please input moeny num:")
-shop_list_list=[]
+shop_list_list = []
 while True:
-    for index,p in enumerate(shop_list):
-        print index,p[0],p[1]
+    for index, p in enumerate(shop_list):
+        print index, p[0], p[1]
     print "input is num;input quit"
     choice = raw_input("choose sth to buy:").strip()
     if choice.isdigit():
         choice = int(choice)
-        p_price=shop_list[choice][1]
+        p_price = shop_list[choice][1]
         if p_price <= slary:
             shop_list_list.append(shop_list[choice])
             print shop_list_list    
@@ -25,8 +25,8 @@ while True:
             print "\033[41;1mMoney is not enough, try sth else dx \033[0m"
     elif choice == 'quit':
         print '--------shopping list--------'
-        for k,v in enumerate(shop_list_list):
-            print k,v 
-        print "your current balance is \033[41;1m%s\033[0m "% slary
+        for k, v in enumerate(shop_list_list):
+            print k, v 
+        print "your current balance is \033[41;1m%s\033[0m " % slary
         print '---------bye------'
         break
